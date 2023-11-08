a1bb7afdf04c355b61a8ef649b9b8e2ee5a1dcbe
Thomas Topfstedt <thotop100@gmail.com>
2023-11-08 14:49:08 -0300

    add

--- server.js
+++ server.js
@@ -34,6 +34,13 @@ app.post('/subtrair', (req, res) => {
     res.send(`O resultado é ${resultado}`);
 });
 
+app.post('/exp', (req, res) => {
+    const valor1 = parseFloat(req.body.valor1);
+    const valor2 = parseFloat(req.body.valor2);
+    const resultado = valor1^valor2;
+    res.send(`O resultado é ${resultado}`);
+});
+
 const PORT = 25565;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
