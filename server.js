281fbcf11250cf50f09ccf2afca15dd791705ad9
Thomas Topfstedt <thotop100@gmail.com>
2023-11-08 14:32:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,27 @@ app.post('/multiplicar', (req, res) => {
     res.send(`O resultado é ${resultado}`);
 });
 
+app.post('/dividir', (req, res) => {
+    const valor1 = parseFloat(req.body.valor1);
+    const valor2 = parseFloat(req.body.valor2);
+    const resultado = valor1 / valor2;
+    res.send(`O resultado é ${resultado}`);
+});
+
+app.post('/somar', (req, res) => {
+    const valor1 = parseFloat(req.body.valor1);
+    const valor2 = parseFloat(req.body.valor2);
+    const resultado = valor1 + valor2;
+    res.send(`O resultado é ${resultado}`);
+});
+
+app.post('/subtrair', (req, res) => {
+    const valor1 = parseFloat(req.body.valor1);
+    const valor2 = parseFloat(req.body.valor2);
+    const resultado = valor1 - valor2;
+    res.send(`O resultado é ${resultado}`);
+});
+
 const PORT = 25565;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
