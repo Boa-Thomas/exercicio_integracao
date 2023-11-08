b6e97b761968cc80a4fe6e22c4bdb842bbf155c1
Thomas Topfstedt <thotop100@gmail.com>
2023-11-08 14:30:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Include CORS module
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
 
-app.post('/multiplicar', (req, res) => {    
-    const valor1 = parseFloat(req.body.valor1);    
-    const valor2 = parseFloat(req.body.valor2);    
-    const resultado = valor1 * valor2;    
+app.post('/multiplicar', (req, res) => {
+    const valor1 = parseFloat(req.body.valor1);
+    const valor2 = parseFloat(req.body.valor2);
+    const resultado = valor1 * valor2;
     res.send(`O resultado é ${resultado}`);
 });
 
 const PORT = 25565;
-app.listen(PORT, () => {    
+app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
