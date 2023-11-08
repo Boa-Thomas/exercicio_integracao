faee83e815b591558564c4ce9aa47a6bf4162d30
Thomas Topfstedt <thotop100@gmail.com>
2023-11-08 14:19:38 -0300

    fix?

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/multiplicar', (req, res) => {
-    const valor1 = parseFloat(req.body.valor1);
-    const valor2 = parseFloat(req.body.valor2);
-    const resultado = valor1 * valor2;
+app.post('/multiplicar', (req, res) => {    
+    const valor1 = parseFloat(req.body.valor1);    
+    const valor2 = parseFloat(req.body.valor2);    
+    const resultado = valor1 * valor2;    
     res.send(`O resultado é ${resultado}`);
 });
 
 const PORT = 25565;
-app.listen(PORT, () => {
+app.listen(PORT, () => {    
     console.log(`Servidor rodando na porta ${PORT}`);
 });
